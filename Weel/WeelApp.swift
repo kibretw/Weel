d583ed48a9d51c6b8f94afbf156966b6ba28601b
kibretw <kibretwolde@gmail.com>
2025-03-06 20:20:44 -0500

    Initial Commit

---
+++ Weel/WeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeelApp.swift
+//  Weel
+//
+//  Created by Kibret Woldemichael on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
